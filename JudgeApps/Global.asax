fbdf2e058af13cba42b291e64bb20bbc0f5e2dc2
RAY ADDERLEY JM. GINING <ray_adderley@staf.uitm.edu.my>
2017-09-28 13:19:41 +0800

    Add project files.

---
+++ JudgeApps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JudgeApps.MvcApplication" Language="C#" %>
---
+++ JudgeApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JudgeApps.Startup))]
+namespace JudgeApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
